9309c5471ccc26d1f005c14deb156683e12fc0d4
JOSHNAVIDEVI <djoshnavi@gmail.com>
2023-09-12 17:20:44 +0530

    Added a new router of PrivacyPolicy

---
+++ src/components/privacy.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Privacy = () => {
+    return (
+        <div class="px-4 md:px-8 lg:px-12 xl:px-16 my-10">
+            <h1 class="font-extrabold mb-4 text-2xl md:text-4xl lg:text-5xl dark:text-white">Privacy Policy</h1>
+            <p class="text-base md:text-xl lg:text-2xl xl:text-2xl">
+                Spotmies ("we," "our," or "us") is committed to protecting your privacy. This Privacy Policy outlines how we collect, use, disclose, and safeguard your personal information when you use our website or engage with our services. By accessing or using our website and services, you consent to the practices described in this Privacy Policy.</p>
+            <h2 class="px-1.5 text-2xl font-bold mt-10" >Information We Collect</h2>
+            <div class="text-xl mt-3 ">
+                <p class="text-xl"><b>1.Personal Information:</b> We may collect personal information, such as your name, email address, phone number, and postal address, when you voluntarily provide it to us through forms, emails, or other means.<br />
+                <b>2. Usage Information:</b> We may collect non-personal information about how you interact with our website, including your IP address, browser type, operating system, referring URLs, and pages visited. We use this information for analytics and to improve our website's functionality.</p>
+            </div>
+            <h2 class=" text-2xl font-bold mt-10">How We Use Your Information</h2>
+            <p class="text-xl">
+                We may use your personal and non-personal information for the following purposes:<br />
+
+                1. <b>Communication:</b> To respond to your inquiries, provide information, and communicate with you about our services.<br />
+
+                2. <b>Service Improvement:</b> To analyze user preferences and behavior to enhance our website and services.<br />
+
+                3. <b>Marketing:</b> To send you updates, newsletters, and promotional materials about our services. You can opt out of these communications at any time.<br />
+
+                4. <b>Legal Compliance:</b> To comply with applicable laws, regulations, or legal processes.</p>
+            <h2 class="text-2xl font-bold mt-10">Information Sharing</h2>
+            <p class="text-xl">
+                We do not sell, rent, or trade your personal information to third parties. However, we may share your information with trusted service providers who assist us in operating our website and delivering our services. These service providers are contractually obligated to protect your information.
+
+                We may also disclose your information if required by law or in response to valid legal requests, such as a court order or government investigation.</p>
+            <h2 class="text-2xl font-bold mt-10">Data Security</h2>
+            <p class="text-xl">We employ reasonable administrative, technical, and physical safeguards to protect your information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.</p>
+            <h2 class="text-2xl font-bold mt-10">Your Choices</h2>
+            <p class="text-xl"><b>1. Access:</b> You can request access to your personal information and ask for it to be corrected, updated, or deleted.
+
+                <b>2. Opt-out:</b> You can opt out of receiving marketing communications from us.
+                <b>
+                    3. Cookies:</b> You can manage your cookie preferences through your browser settings.
+            </p>
+            <h2 class="text-2xl font-bold mt-10">Children's Privacy</h2>
+            <p class="text-xl">Spotmies welcomes users of all ages, including children, to access and use our services. We are committed to providing a safe and enjoyable online environment for children, and we do not knowingly collect personal information from children under the age of 13. Our services are designed to be child-friendly, and we take steps to ensure that children can use our website without encountering any issues.</p>
+
+            <h2 class="text-2xl font-bold mt-10">Changes to this Privacy Policy</h2>
+            <p class="text-xl mt-4">
+                We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes by posting a notice on our website or by other means.
+            </p>
+            <h2 class="text-2xl font-bold mt-10">Contact Us</h2>
+            <p class="text-xl mt-5">If you have any questions, concerns, or requests regarding this Privacy Policy or the way we handle your personal information, please contact us at:</p>
+            <p class="text-xl mt-4">
+                info@spotmies.com<br />
+                +91 8341980196<br />
+                www.spotmies.com</p>
+            <p class="text-xl mt-4">By using our website and services, you agree to this Privacy Policy. If you do not agree with our practices, please do not use our website or services.</p>
+
+
+
+
+        </div>
+    );
+};
+
+export default Privacy;
