005833573a21b3b98db9298e52a32914a9e045a5
Sirisha Pedapudi <your.email@example.com>
2026-01-21 16:06:25 +0530

    Update footer copyright year to 2026 and change image sources in work.json to new storage location

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -142,7 +142,7 @@ export default function FooterBar() {
           </div>
           <div className="w-full flex flex-col md:flex-row items-center justify-between">
             <div className="text-sm lg:text-lg text-gray-400">
-              © 2024 Spotmies LLP. All Rights Reserved.
+              © 2026 Spotmies LLP. All Rights Reserved.
             </div>
             <div className="flex space-x-4 md:pt-0 lg:pt-0 pt-4">
               <a
